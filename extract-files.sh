56abb2600430c0ac09536c7fa4a7dd422670ac0d
Marlon Alkan <marlon@snicco.de>
2022-04-24 13:47:20 +0200

    A3: add extract-files.sh

---
+++ extract-files.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=A3
+VENDOR=umidigi
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+KANG=
+SECTION=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+#function blob_fixup() {
+#    case "${1}" in
+#        system_ext/etc/init/dpmd.rc)
+#            sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
+#            ;;
+#        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | system_ext/etc/permissions/telephonyservice.xml)
+#            sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
+#            ;;
+#        system_ext/etc/permissions/qcrilhook.xml)
+#            sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
+#            ;;
+#        system_ext/lib64/libdpmframework.so)
+#            "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
+#            ;;
+#        vendor/bin/mlipayd@1.1)
+#           "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+#            ;;
+#        vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
+#            "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+#            sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
+#            ;;
+#        lib/libwfdaudioclient.so)
+#            "${PATCHELF}" --set-soname "libwfdaudioclient.so" "${2}"
+#            ;;
+#        lib/libwfdmediautils.so)
+#            "${PATCHELF}" --set-soname "libwfdmediautils.so" "${2}"
+#            ;;
+#        lib/libwfdmmsink.so)
+#            "${PATCHELF}" --add-needed "libwfdaudioclient.so" "${2}"
+#            "${PATCHELF}" --add-needed "libwfdmediautils.so" "${2}"
+#            ;;
+#    esac
+#}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+"${MY_DIR}/setup-makefiles.sh"
